0de73510fd7f30742e8badca3aad0dde33f15f30
Navid Dezashibi <dezashibi@gmail.com>
2024-02-27 15:42:43 +0400

    Update Hud.svelte

--- src/lib/components/Game/Hud.svelte
+++ src/lib/components/Game/Hud.svelte
@@ -1,23 +1,13 @@
 <script lang="ts">
 	import { logoCount } from '$lib/stores';
-	let count = 0;
 </script>
 
 <div>
 	<p>some sort of hud stuff goes here</p>
 </div>
-
+<h2>Logo count: {$logoCount}</h2>
+<input type="range" min="0" max="40" bind:value={$logoCount} />
 <div>
-	<div>
-		<h2>Logo count: {$logoCount}</h2>
-		<div>
-			<input type="range" min="0" max="40" bind:value={$logoCount} />
-			<div>
-				<button on:click={() => ($logoCount -= 1)}>-</button>
-				<button on:click={() => ($logoCount += 1)}>+</button>
-			</div>
-		</div>
-	</div>
+	<button on:click={() => ($logoCount -= 1)}>-</button>
+	<button on:click={() => ($logoCount += 1)}>+</button>
 </div>
-
-<div />
