7fdf986867232e34deed47b12b871e9f69f2b3b2
Navid Dezashibi <dezashibi@gmail.com>
2024-03-05 11:37:25 +0400

    Update config.ts

--- src/lib/phaser/config.ts
+++ src/lib/phaser/config.ts
@@ -4,10 +4,16 @@ import LoadingSplash from './scenes/Splash';
 import PreloaderScene from './scenes/PreloaderScene';
 import GameScene from './scenes/GameScene';
 
+export const gameConfig = {
+    width: 360,
+    height: 640,
+}
+
 export const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.WEBGL,
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: window.innerWidth < gameConfig.width ? gameConfig.width : window.innerWidth,
+    height: window.innerHeight < gameConfig.height ? gameConfig.height : window.innerHeight,
+    title: 'My Game',
     physics: {
         default: 'matter',
         matter: {
