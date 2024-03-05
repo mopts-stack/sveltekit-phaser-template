5b1382844fd781ee7afde3c60a73716d1e8e9ad1
Navid Dezashibi <dezashibi@gmail.com>
2024-03-05 12:45:07 +0400

    Update Phaser.svelte

--- src/lib/components/Game/Phaser.svelte
+++ src/lib/components/Game/Phaser.svelte
@@ -13,8 +13,35 @@
 	});
 </script>
 
-<div>
+<!-- Parent container with relative positioning -->
+<div class="container">
+	<!-- Hud component with absolute positioning to overlay on top of the canvas -->
+	<div class="hud">
+		<Hud />
+	</div>
+
+	<!-- Canvas container -->
 	<canvas bind:this={gameContainer} />
 </div>
 
-<Hud />
+
+<style>
+	.container {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		user-select: none; /* Disable text selection */
+	}
+
+	.container .hud {
+		position: absolute;
+		z-index: 1;
+		color: white;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); /* Text shadow for contrast */
+		background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent background */
+		padding: 0 10px; /* Padding for text */
+	}
+</style>
