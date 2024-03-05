126264d6110ceef1d55ec37e0aa1cb97e0f935aa
Navid Dezashibi <dezashibi@gmail.com>
2024-03-05 12:51:59 +0400

    Update Hud.svelte

--- src/lib/components/Game/Hud.svelte
+++ src/lib/components/Game/Hud.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <div>
+	<h2>Logo count: {$logoCount}</h2>
 	<p>some sort of hud stuff goes here</p>
 </div>
-<h2>Logo count: {$logoCount}</h2>
-<input type="range" min="0" max="40" bind:value={$logoCount} />
 <div>
+	<input type="range" min="0" max="40" bind:value={$logoCount} />
 	<button on:click={() => ($logoCount -= 1)}>-</button>
 	<button on:click={() => ($logoCount += 1)}>+</button>
 </div>
