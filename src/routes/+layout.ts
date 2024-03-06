46916ab3c0cea01f21f8fae7d6af3096051550bd
Navid Dezashibi <dezashibi@gmail.com>
2024-03-06 09:00:17 +0400

    Update +layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,5 +3,5 @@ import { inject } from '@vercel/analytics';
 
 inject({ mode: dev ? 'development' : 'production' });
 
+// you can move it to the layouts with game in it only
 export const ssr = false;
-export const prerender = true;
